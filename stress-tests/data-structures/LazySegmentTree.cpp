8e2a106bd5474dfd481dcdb80aa5ab109acafdf2
user <yarinsaraga1@gmail.com>
2025-05-30 15:21:34 +0300

    masheo

--- stress-tests/data-structures/LazySegmentTree.cpp
+++ stress-tests/data-structures/LazySegmentTree.cpp
@@ -8,18 +8,20 @@ int ra() {
 	R += 1231;
 	return (int)(R >> 1);
 }
-
 volatile int res;
-int main() {
+signed main() {
 	int N = 10;
 	vi v(N);
 	iota(all(v), 0);
 	random_shuffle(all(v), [](int x) { return ra() % x; });
-	Node* tr = new Node(v,0,N);
+	Tree tree(N);
+    rep(i, 0, N){
+        tree.set(i,i+1,v[i]);
+    }
 	rep(i,0,N) rep(j,0,N) if (i <= j) {
-		int ma = -inf;
-		rep(k,i,j) ma = max(ma, v[k]);
-		assert(ma == tr->query(i,j));
+		int ma = 0;
+		rep(k,i,j) ma = ma + v[k];
+		assert(ma == tree.query(i,j));
 	}
 	rep(it,0,1000000) {
 		int i = ra() % (N+1), j = ra() % (N+1);
@@ -28,17 +30,17 @@ int main() {
 
 		int r = ra() % 100;
 		if (r < 30) {
-			::res = tr->query(i, j);
-			int ma = -inf;
-			rep(k,i,j) ma = max(ma, v[k]);
+			::res = tree.query(i, j);
+			int ma = 0;
+			rep(k,i,j) ma = ma + v[k];
 			assert(ma == ::res);
 		}
 		else if (r < 70) {
-			tr->add(i, j, x);
+			tree.add(i, j, x);
 			rep(k,i,j) v[k] += x;
 		}
 		else {
-			tr->set(i, j, x);
+			tree.set(i, j, x);
 			rep(k,i,j) v[k] = x;
 		}
 	}
