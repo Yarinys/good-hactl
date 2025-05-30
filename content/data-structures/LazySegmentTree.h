59059fc57e4f15c8bdd22c7fbe5a5182dc79ddcf
Yarinys <yarinsaraga1@gmail.com>
2025-05-30 14:54:26 +0300

    Update LazySegmentTree.h

--- content/data-structures/LazySegmentTree.h
+++ content/data-structures/LazySegmentTree.h
@@ -1,8 +1,4 @@
 /**
- * Author: Simon Lindholm
- * Date: 2016-10-08
- * License: CC0
- * Source: me
  * Description: Segment tree with ability to add or set values of large intervals, and compute max of intervals.
  * Can be changed to other things e.g. min, max, sum, xor, gcd, lcm (by replacing "max" function, replacing "val" neutral value, and sometimes other things specified).
  * Use with a bump allocator for better performance, and SmallPtr or implicit indices to save memory.
@@ -11,64 +7,77 @@
  * Status: stress-tested a bit
  */
 #pragma once
-
 #include "../various/BumpAllocator.h"
+//  1. add(l, r, x): add x to every element in range [l, r)
+//  2. set(l, r, x): set every element in range [l, r) to x
+//  3. query(l, r): find the sum of values in range [l, r)
+struct Tree {
+    int n;
+    vector<ll> sum;
+    vector<ll> lazy_add;
+    vector<ll> lazy_set;
 
-const int inf = 1e9;
-struct Node {
-	Node *l = 0, *r = 0;
-	int lo, hi, mset = inf, madd = 0;
-	int val = -inf; // min: inf, max: -inf, sum: 0 or [init_value * (hi - lo) -> in constructor], gcd: 1 (init_value), lcm: 1 (init_value)
-	Node(int lo,int hi):lo(lo),hi(hi){} // sparse segment tree (use for large intervals of default value "val")
-	Node(vi& v, int lo, int hi) : lo(lo), hi(hi) {
-		// build segment tree from v - [lo, hi)
-		if (lo + 1 < hi) {
-			int mid = lo + (hi - lo)/2;
-			l = new Node(v, lo, mid); r = new Node(v, mid, hi);
-			val = max(l->val, r->val);
-		}
-		else val = v[lo];
-	}
-	int query(int L, int R) { // [L, R)
-		if (R <= lo || hi <= L) return -inf; // CHANGE THIS (out of range)
-		if (L <= lo && hi <= R) return val;
-		push();
-		return max(l->query(L, R), r->query(L, R));
-	}
-	void set(int L, int R, int x) { // [L, R)
-		if (R <= lo || hi <= L) return;
-		if (L <= lo && hi <= R) {
-			// CHANGE THIS (update covers node)
-			mset = x;
-			val = x; // e.g. for sum: x * (hi - lo), for xor: x * ((hi - lo) & 1)
-			madd = 0;
-		}
-		else {
-			push(), l->set(L, R, x), r->set(L, R, x);
-			val = max(l->val, r->val);
-		}
-	}
-	void add(int L, int R, int x) { // [L, R)
-		if (R <= lo || hi <= L) return;
-		if (L <= lo && hi <= R) {
-			// CHANGE THIS (update covers node)
-			if (mset != inf) mset += x;
-			else madd += x;
-			val += x; // e.g. for sum: x * (hi - lo), xor/gcd/lcm too hard
-		}
-		else {
-			push(), l->add(L, R, x), r->add(L, R, x);
-			val = max(l->val, r->val);
-		}
-	}
-	void push() {
-		if (!l) {
-			int mid = lo + (hi - lo)/2;
-			l = new Node(lo, mid); r = new Node(mid, hi);
-		}
-		if (mset != inf)
-			l->set(lo,hi,mset), r->set(lo,hi,mset), mset = inf;
-		else if (madd)
-			l->add(lo,hi,madd), r->add(lo,hi,madd), madd = 0;
-	}
+    Tree(int n) : n(n), sum(n * 4), lazy_add(n * 4), lazy_set(n * 4) {}
+
+    void add(int s, int l, int r, int ql, int qr, int x) {
+        if (r <= ql || qr <= l) return;
+        if (ql <= l && r <= qr) {
+            apply_add(s, r - l, x);
+            return;
+        }
+        push(s, r - l);
+        int m = (l + r) / 2;
+        add(s * 2 + 0, l, m, ql, qr, x);
+        add(s * 2 + 1, m, r, ql, qr, x);
+        sum[s] = sum[s * 2] + sum[s * 2 + 1];
+    }
+
+    void set(int s, int l, int r, int ql, int qr, int x) {
+        if (r <= ql || qr <= l) return;
+        if (ql <= l && r <= qr) {
+            apply_set(s, r - l, x);
+            return;
+        }
+        push(s, r - l);
+        int m = (l + r) / 2;
+        set(s * 2 + 0, l, m, ql, qr, x);
+        set(s * 2 + 1, m, r, ql, qr, x);
+        sum[s] = sum[s * 2] + sum[s * 2 + 1];
+    }
+
+    ll query(int s, int l, int r, int ql, int qr) {
+        if (r <= ql || qr <= l) return 0;
+        if (ql <= l && r <= qr) return sum[s];
+        push(s, r - l);
+        int m = (l + r) / 2;
+        return query(s * 2, l, m, ql, qr) + query(s * 2 + 1, m, r, ql, qr);
+    }
+
+    void push(int s, int len) {
+        if (lazy_set[s]) {
+            apply_set(s * 2, len / 2, lazy_set[s]);
+            apply_set(s * 2 + 1, (len + 1) / 2, lazy_set[s]);
+            lazy_set[s] = 0;
+        }
+        if (lazy_add[s]) {
+            apply_add(s * 2, len / 2, lazy_add[s]);
+            apply_add(s * 2 + 1, (len + 1) / 2, lazy_add[s]);
+            lazy_add[s] = 0;
+        }
+    }
+
+    void apply_add(int s, int len, ll x) {
+        sum[s] += x * len;
+        lazy_add[s] += x;
+    }
+
+    void apply_set(int s, int len, ll x) {
+        sum[s] = x * len;
+        lazy_add[s] = 0;
+        lazy_set[s] = x;
+    }
+
+    void add(int l, int r, int x) { add(1, 0, n, l, r, x); }
+    void set(int l, int r, int x) { set(1, 0, n, l, r, x); }
+    ll query(int l, int r) { return query(1, 0, n, l, r); }
 };
